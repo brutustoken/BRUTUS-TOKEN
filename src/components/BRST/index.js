ee3a4a3730b519741d383f645455de43730f2404
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2023-03-27 20:00:52 -0500

    ajustes contratos y charts

--- src/components/BRST/index.js
+++ src/components/BRST/index.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
-import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
 
 import * as am5 from "@amcharts/amcharts5";
 import * as am5xy from "@amcharts/amcharts5/xy";
-
-import CrowdFunding from "./StakingCrowdFunding";
-import Oficina from "./StakingOficina";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
 
 
 export default class Staking extends Component {
@@ -13,18 +10,344 @@ export default class Staking extends Component {
     super(props);
 
     this.state = {
-      minCompra: 10
+
+      minCompra: 10,
+      minventa: 1,
+      deposito: "Cargando...",
+      wallet: "Cargando...",
+      valueBRUT: "",
+      valueUSDT: "",
+      value: "",
+      cantidad: 0,
+      tiempo: 0,
+      enBrutus: 0,
+      tokensEmitidos: 0,
+      enPool: 0,
+      solicitado: 0,
+      data: []
 
     };
 
     this.grafico = this.grafico.bind(this);
+
+    this.compra = this.compra.bind(this);
+    this.venta = this.venta.bind(this);
+    this.estado = this.estado.bind(this);
+
+    this.handleChangeBRUT = this.handleChangeBRUT.bind(this);
+    this.handleChangeUSDT = this.handleChangeUSDT.bind(this);
+
+    this.llenarBRUT = this.llenarBRUT.bind(this);
+    this.llenarUSDT = this.llenarUSDT.bind(this);
+
+    this.consultarPrecio = this.consultarPrecio.bind(this);
+    this.completarSolicitud = this.completarSolicitud.bind(this);
+
   }
+ 
+
+  
   componentDidMount() {
+    document.title = "Brutus Finance | BRST"
     this.grafico();
+
+    this.estado();
+    setInterval(() => {
+      this.estado()
+
+    },3*1000);
   }
 
+  componentWillUnmount() {
+    if (this.root) {
+      this.root.dispose();
+    }
+  }
+
+  handleChangeBRUT(event) {
+    this.setState({valueBRUT: event.target.value});
+  }
+
+  handleChangeUSDT(event) {
+    this.setState({valueUSDT: event.target.value});
+  }
+
+  llenarBRUT(){
+    document.getElementById('amountBRUT').value = this.state.balanceBRUT;
+    this.setState({valueBRUT: this.state.balanceBRUT});
+    
+  }
+
+  llenarUSDT(){
+    document.getElementById('amountUSDT').value = this.state.balanceUSDT;
+    this.setState({valueUSDT: this.state.balanceUSDT});
+  }
+
+  
+
+  async consultarPrecio(){
+
+    var precio = await this.props.contrato.BRST_TRX.RATE().call();
+    precio = parseInt(precio._hex)/10**6;
+
+    this.setState({
+      precioBRUT: precio
+    });
+
+    return precio;
+
+  };
+
+  async completarSolicitud(id, trx){
+
+    await this.props.contrato.BRST_TRX.completarSolicitud(id).send({callValue: trx});
+
+  }
+
+  async estado(){
+
+    var accountAddress =  this.props.accountAddress;
+
+    var aprovadoUSDT = await window.tronWeb.trx.getBalance();
+
+    aprovadoUSDT = aprovadoUSDT/10**6;
+
+    var balanceUSDT = aprovadoUSDT;
+
+    if (aprovadoUSDT > 0) {
+      aprovadoUSDT = "Comprar "; 
+    }else{
+      aprovadoUSDT = "Necesitas TRX para hacer Staking"; 
+      this.setState({
+        valueUSDT: ""
+      })
+    }
+
+    var MIN_DEPOSIT = await this.props.contrato.BRST_TRX.MIN_DEPOSIT().call();
+    MIN_DEPOSIT = parseInt(MIN_DEPOSIT._hex)/10**6;
+
+    var aprovadoBRUT = await this.props.contrato.BRST.allowance(accountAddress,this.props.contrato.BRST_TRX.address).call();
+    aprovadoBRUT = parseInt(aprovadoBRUT._hex);
+
+    var balanceBRUT = await this.props.contrato.BRST.balanceOf(accountAddress).call();
+    balanceBRUT = parseInt(balanceBRUT._hex)/10**6;
+
+    if (aprovadoBRUT > 0) {
+      aprovadoBRUT = "Vender ";
+    }else{
+      aprovadoBRUT = "Aprobar intercambio";
+      this.setState({
+        valueBRUT: ""
+      })
+    }
+
+    var precioBRUT =  await this.consultarPrecio();
+
+    var deposito = await this.props.contrato.BRST_TRX.todasSolicitudes(accountAddress).call();
+
+    var tiempo = await this.props.contrato.BRST_TRX.TIEMPO().call();
+
+    tiempo = parseInt(tiempo._hex)*1000;
+
+    console.log(deposito);
+
+    var misDepositos = [];
+    var id;
+    
+    for (let index = 0; index < deposito.brst.length; index++) {
+      if (!deposito.completado[index]) {
+        id = parseInt(deposito.id[index]._hex);
+        misDepositos.push(<div className="col-lg-12" key={"mis-"+id}>
+          <p># {id} | {parseInt(deposito.brst[index]._hex)/10**6} BRST -&gt; {parseInt(deposito.trxx[index]._hex)/10**6} TRX  {" "}
+          <button type="button" className="btn btn-warning" onClick={() => this.completarSolicitud(parseInt(deposito.id[index]._hex), 0)}>Cancelar</button></p>
+          <hr></hr>
+        </div>)
+      }
+      
+    }
+
+    var deposits = await this.props.contrato.BRST_TRX.solicitudesPendientesGlobales().call();
+
+    var globDepositos = [];
+
+    var pen;
+    
+    for (let index = 0; index < deposits.length; index++) {
+
+      pen = await this.props.contrato.BRST_TRX.verSolicitudPendiente(parseInt(deposits[index]._hex)).call();
+
+      globDepositos[index] = (<div className="col-lg-12" key={"glob"+parseInt(deposits[index]._hex)}>
+          <p># {parseInt(deposits[index]._hex)} | {parseInt(pen[3]._hex)/10**6} BRST -&gt; {parseInt(pen[2]._hex)/10**6} TRX  {" "}
+          <button type="button" className="btn btn-prymary" onClick={async() => {
+            var local = await this.props.contrato.BRST_TRX.verSolicitudPendiente(parseInt(deposits[index]._hex)).call();
+            this.completarSolicitud(parseInt(deposits[index]._hex),parseInt(local[2]._hex));
+            }}>Completar</button></p>
+          <hr></hr>
+        </div>)
+      
+      
+    }
+
+
+    var enBrutus = await this.props.contrato.BRST_TRX.TRON_BALANCE().call();
+    var tokensEmitidos = await this.props.contrato.BRST.totalSupply().call();
+    var enPool = await this.props.contrato.BRST_TRX.TRON_PAY_BALANCE().call();
+    var solicitado = await this.props.contrato.BRST_TRX.TRON_SOLICITADO().call();
+    var solicitudes = await this.props.contrato.BRST_TRX.index().call();
+
+    
+
+    //console.log(tokensEmitidos);
+    this.setState({
+      minCompra: MIN_DEPOSIT,
+      globDepositos: globDepositos,
+      misDepositos: misDepositos,
+      depositoUSDT: aprovadoUSDT,
+      depositoBRUT: aprovadoBRUT,
+      balanceBRUT: balanceBRUT,
+      balanceUSDT: balanceUSDT,
+      wallet: accountAddress,
+      precioBRUT: precioBRUT,
+      espera: tiempo,
+      enBrutus: parseInt(enBrutus._hex)/10**6,
+      tokensEmitidos: parseInt(tokensEmitidos._hex)/10**6,
+      enPool: parseInt(enPool._hex)/10**6,
+      solicitado: parseInt(solicitado._hex)/10**6,
+      solicitudes: parseInt(solicitudes._hex),
+    });
+
+  }
+
+  async compra() {
+
+
+    const { minCompra } = this.state;
+
+    var amount = document.getElementById("amountUSDT").value;
+    amount = parseFloat(amount);
+    amount = parseInt(amount*10**6);
+
+    var aprovado = await window.tronWeb.trx.getBalance();
+
+    if ( aprovado >= amount ){
+
+
+        if ( amount >= minCompra){
+
+          document.getElementById("amountUSDT").value = "";
+
+          await this.props.contrato.BRST_TRX.staking().send({callValue: amount});
+
+        }else{
+          window.alert("Please enter an amount greater than "+minCompra+" USDT");
+          document.getElementById("amountUSDT").value = minCompra;
+        }
+
+
+
+    }else{
+
+        if ( amount > aprovado) {
+          if (aprovado <= 0) {
+            document.getElementById("amountUSDT").value = minCompra;
+            window.alert("You do not have enough funds in your account you place at least 10 USDT");
+          }else{
+            document.getElementById("amountUSDT").value = minCompra;
+            window.alert("You must leave 50 TRX free in your account to make the transaction");
+          }
+
+
+
+        }else{
+
+          document.getElementById("amountUSDT").value = amount;
+          window.alert("You must leave 50 TRX free in your account to make the transaction");
+
+        }
+    }
+
+    this.llenarUSDT();
+
+  };
+
+  async venta() {
+
+
+    const { minventa } = this.state;
+
+    var amount = document.getElementById("amountBRUT").value;
+    amount = parseFloat(amount);
+    amount = parseInt(amount*10**6);
+
+    var accountAddress =  await window.tronWeb.trx.getAccount();
+    accountAddress = window.tronWeb.address.fromHex(accountAddress.address);
+
+    var aprovado = await this.props.contrato.BRST.allowance(accountAddress,this.props.contrato.BRST_TRX.address).call();
+    aprovado = parseInt(aprovado._hex);
+
+    if ( aprovado >= amount ){
+
+
+        if ( amount >= minventa){
+
+          document.getElementById("amountBRUT").value = "";
+
+          var pass = window.confirm("Tu solicitud generará una orden de venta para tus BRST esperando a que sea completada por la comunidad");
+          if(pass){await this.props.contrato.BRST_TRX.solicitudRetiro(amount).send()};
+
+          //window.alert("Estamos actualizando a la version 3 del contrato de liquidez por favor contacta atravez de telegram para intercambiar tus BRST por TRX, estamos mejorando nustro sistema ;)");
+
+        }else{
+          window.alert(`ingrese un valor mayor a ${minventa} BRST`);
+          document.getElementById("amountBRUT").value = minventa;
+        }
+
+
+
+    }else{
+
+
+        if (aprovado <= 0) {
+          await this.props.contrato.BRST.approve(this.props.contrato.BRST_TRX.address, "115792089237316195423570985008687907853269984665640564039457584007913129639935").send();
+        }
+
+        if ( amount > aprovado) {
+          if (aprovado <= 0) {
+            document.getElementById("amountBRUT").value = minventa;
+            window.alert("You do not have enough funds in your account you place at least "+minventa+" USDT");
+          }else{
+            document.getElementById("amountBRUT").value = minventa;
+            window.alert("You must leave 50 TRX free in your account to make the transaction");
+          }
+
+
+
+        }else{
+
+          document.getElementById("amountBRUT").value = minventa;
+          window.alert("You must leave 50 TRX free in your account to make the transaction");
+
+        }
+
+    }
+
+    this.llenarBRUT();
+
+  };
+
+  async retiro() {
+
+    if (Date.now() >= this.state.tiempo && this.state.tiempo-this.state.espera !== 0) {
+      await this.props.contrato.BRST_TRX.retirar().send();
+    }else{
+      window.alert("todavia no es tiempo de retirar");
+    }
+
+
+  };
+
   async grafico() {
-    let root = am5.Root.new("chartdiv");
+    const root = am5.Root.new("chartdiv");
 
     root.setThemes([
       am5themes_Animated.new(root)
@@ -96,12 +419,13 @@ export default class Staking extends Component {
         })
       });
     });
-
-    let data = (await (await fetch("https://chainlist.tk/api/v1/chartdata/brst")).json()).Data
+    var data = (await (await fetch("https://chainlist.tk/api/v1/chartdata/brst?dias=30")).json()).Data
     series.data.setAll(data);
 
     series.appear(1000);
     chart.appear(1000, 100);
+
+    this.root = root;
   }
 
   render() {
@@ -120,13 +444,10 @@ export default class Staking extends Component {
                 <div className="ms-3">
                   <h2 className="font-w600 text-black mb-0 title">Tron Staking</h2>
                   <p className="font-w600 text-black sub-title">BRST</p>
-                  <span>1 BRST = 1.250 TRX</span>
+                  <span>1 BRST = {(this.state.enBrutus/this.state.tokensEmitidos).toFixed(3)} TRX</span>
                 </div>
               </div>
               <p className="fs-14">Dash is an open source cryptocurrency. It is an altcoin that was forked from the Bitcoin protocol. It is also a decentralized autonomous organization (DAO) run by a subset of its users, which are called "masternodes". The currency permits transactions that can be untraceable.</p>
-            </div>
-            <div className="card-footer border-0 p-0 caret">
-              <a href="coin-details.html" className="btn-link"><i className="fa fa-caret-down" aria-hidden="true"></i></a>
             </div>
           </div>
         </div>
@@ -138,18 +459,18 @@ export default class Staking extends Component {
                 <div className="col-lg-4 col-xxl-4 col-sm-4 d-flex flex-wrap align-items-center">
                   <div className="px-2 info-group">
                     <p className="fs-18 mb-1">Precio TRX</p>
-                    <h2 className="fs-28 font-w600 text-black">1.250801</h2>
+                    <h2 className="fs-28 font-w600 text-black">{(this.state.enBrutus/this.state.tokensEmitidos).toFixed(6)}</h2>
                   </div>
                 </div>
                 <div className="d-flex col-lg-8 col-xxl-8 col-sm-8 align-items-center mt-sm-0 mt-3 justify-content-end">
 
                   <div className="px-2 info-group">
                     <p className="fs-14 mb-1">Respaldo TRX</p>
-                    <h3 className="fs-20 font-w600 text-black">472,067.48</h3>
+                    <h3 className="fs-20 font-w600 text-black">{this.state.enBrutus}</h3>
                   </div>
                   <div className="px-2 info-group">
-                    <p className="fs-14 mb-1">Market Cap</p>
-                    <h3 className="fs-20 font-w600 text-black">$30279.92</h3>
+                    <p className="fs-14 mb-1">BRST Circulando</p>
+                    <h3 className="fs-20 font-w600 text-black">{this.state.tokensEmitidos}</h3>
                   </div>
                 </div>
               </div>
@@ -210,8 +531,6 @@ export default class Staking extends Component {
             </div>
           </div>
         </div>
-        <CrowdFunding />
-        <Oficina />
       </div>
 
     );
