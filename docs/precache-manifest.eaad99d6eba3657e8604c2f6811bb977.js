29f3f287f7607e129ed711d4fe20015c351a63b1
STC <32532691+stevenstc@users.noreply.github.com>
2024-04-29 07:51:46 -0500

    cs

---
+++ docs/precache-manifest.eaad99d6eba3657e8604c2f6811bb977.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774a2034ed789505c87fa615e8aa4dbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f0fccf016bc274bc1a7",
+    "url": "/static/js/2.068044ff.chunk.js"
+  },
+  {
+    "revision": "99535160bb24520f8a17b68fc511534d",
+    "url": "/static/js/2.068044ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d2e577efeb4bc866654",
+    "url": "/static/js/main.5bbe2368.chunk.js"
+  },
+  {
+    "revision": "444a0910a50d246d9ce4",
+    "url": "/static/js/runtime-main.6b408e45.js"
+  }
+]);
