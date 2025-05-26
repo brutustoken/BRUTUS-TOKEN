70ed1f4df18868a8af79bbd884c95876227bafc3
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2025-05-26 11:01:34 -0500

    update tronq

---
+++ docs/precache-manifest.346d5cde0c5b568f3443e5871927b680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb3522c9bf304f611967a5943f4f9a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d902ce0efcb9237ae88c",
+    "url": "/static/js/2.4346ce56.chunk.js"
+  },
+  {
+    "revision": "ddfaa0479da9d80597d72864002d3d7f",
+    "url": "/static/js/2.4346ce56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0737a53e9eb3ec76f3ac",
+    "url": "/static/js/3.cd0c6a59.chunk.js"
+  },
+  {
+    "revision": "9caa4952d66e543f4026",
+    "url": "/static/js/main.fdbfdf00.chunk.js"
+  },
+  {
+    "revision": "ac33ef666c104d1771d3",
+    "url": "/static/js/runtime-main.aba74508.js"
+  }
+]);
