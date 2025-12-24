90a5b1781cf5e5979cfbf3974d8adc00db31d8ce
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2025-12-24 17:06:32 -0500

    otra?

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,17 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Cambiamos el nombre de 'vendor' a 'libs' para evitar conflictos
+            return 'libs';
+          }
+        },
+      }
+    }
   }
 })
