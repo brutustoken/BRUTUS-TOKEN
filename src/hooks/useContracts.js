237b9a74e7c2c04cf6fe50ef32feb53b691aab4a
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2025-12-24 16:57:14 -0500

    Update useContracts.js

--- src/hooks/useContracts.js
+++ src/hooks/useContracts.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import utils from '../utils/utils';
 import config from '../config';
-import abi_PROXY from '../assets/abi/Proxy.json';
 import abi_TOKEN from '../assets/abi/TRC20-USDT.json';
 import abi_POOLBRST from '../assets/abi/PoolBRSTv4.json';
 import abi_BRST_USDT from '../assets/abi/Brst-Usdt.json';
@@ -35,14 +34,14 @@ export const useContracts = (accountAddress) => {
 
         try {
             // Load BRUT contract
-            if (contracts.BRUT === null && config.BRUT !== "") {
+            if (contracts.BRUT === null && config.BRUT) {
                 web3Contracts = await utils.getTronweb(accountAddress, 1);
                 setContracts(prev => ({ ...prev, BRUT: web3Contracts.contract(abi_TOKEN, config.BRUT) }))
 
             }
 
             // Load USDT/USDD contracts
-            if (contracts.USDT === null && config.USDT !== "") {
+            if (contracts.USDT === null && config.USDT) {
                 web3Contracts = await utils.getTronweb(accountAddress, 1);
                 setContracts(prev => ({ ...prev, USDT: web3Contracts.contract(abi_TOKEN, config.USDT) }))
                 setContracts(prev => ({ ...prev, USDD: web3Contracts.contract(abi_TOKEN, config.USDD) }))
@@ -50,60 +49,58 @@ export const useContracts = (accountAddress) => {
             }
 
             // Load BRUT_USDT contract
-            if (contracts.BRUT_USDT === null && config.SC !== "") {
+            if (contracts.BRUT_USDT === null && config.SC) {
+                web3Contracts = await utils.getTronweb(accountAddress);
                 setContracts(prev => ({ ...prev, BRUT_USDT: web3Contracts.contract(abi_BRST_USDT, config.SC) }))
             }
 
             // Load BRST_TRX contract
-            if (contracts.BRST_TRX === null && config.SC2 !== "") {
+            if (contracts.BRST_TRX === null && config.SC2) {
                 web3Contracts = await utils.getTronweb(accountAddress);
-                setContracts(prev => ({ ...prev, BRST_TRX: web3Contracts.contract(abi_TOKEN, config.SC2) }))
+                setContracts(prev => ({ ...prev, BRST_TRX: web3Contracts.contract().at(config.SC2) }))
             }
 
             // Load BRST_TRX_Proxy contract
-            if (contracts.BRST_TRX_Proxy === null && config.ProxySC2 !== "") {
+            if (contracts.BRST_TRX_Proxy === null && config.ProxySC2) {
                 web3Contracts = await utils.getTronweb(accountAddress);
-                setContracts(prev => ({ ...prev, Proxy: web3Contracts.contract(abi_PROXY, config.ProxySC2) }))
                 setContracts(prev => ({ ...prev, BRST_TRX_Proxy: web3Contracts.contract(abi_POOLBRST, config.ProxySC2) }))
             }
 
             // Load BRST_TRX_Proxy_fast contract
-            if (contracts.BRST_TRX_Proxy_fast === null && config.ProxySC3 !== "") {
+            if (contracts.BRST_TRX_Proxy_fast === null && config.ProxySC3) {
                 web3Contracts = await utils.getTronweb(accountAddress);
-                setContracts(prev => ({ ...prev, Proxy_fast: web3Contracts.contract(abi_PROXY, config.ProxySC3) }))
                 setContracts(prev => ({ ...prev, BRST_TRX_Proxy_fast: web3Contracts.contract(abi_SIMPLE_SWAP, config.ProxySC3) }))
             }
 
             // Load BRST contract
-            if (contracts.BRST === null && config.BRST !== "") {
+            if (contracts.BRST === null && config.BRST) {
                 web3Contracts = await utils.getTronweb(accountAddress);
                 setContracts(prev => ({ ...prev, BRST: web3Contracts.contract(abi_TOKEN, config.BRST) }))
             }
 
             // Load BRGY contract
-            if (contracts.BRGY === null && config.BRGY !== "") {
+            if (contracts.BRGY === null && config.BRGY) {
                 web3Contracts = await utils.getTronweb(accountAddress);
                 setContracts(prev => ({ ...prev, BRGY: web3Contracts.contract().at(config.BRGY) }))
 
             }
 
             // Load MBOX contract
-            if (contracts.MBOX === null && config.SC3 !== "") {
+            if (contracts.MBOX === null && config.SC3) {
                 web3Contracts = await utils.getTronweb(accountAddress);
                 setContracts(prev => ({ ...prev, MBOX: web3Contracts.contract().at(config.SC3) }))
             }
 
             // Load BRLT contract
-            if (contracts.ProxyBRLT === null && config.BRLT !== "") {
+            if (contracts.ProxyBRLT === null && config.BRLT) {
                 web3Contracts = await utils.getTronweb(accountAddress, 2);
                 setContracts(prev => ({ ...prev, ProxyBRLT: web3Contracts.contract().at(config.BRLT) }))
 
             }
 
             // Load Lottery contract
-            if (contracts.loteria === null && config.SC4 !== "") {
+            if (contracts.loteria === null && config.SC4) {
                 web3Contracts = await utils.getTronweb(accountAddress, 2);
-                setContracts(prev => ({ ...prev, ProxyLoteria: web3Contracts.contract(abi_PROXY, config.SC4) }))
                 setContracts(prev => ({ ...prev, loteria: web3Contracts.contract(abi_LOTERIA, config.SC4) }))
             }
 
